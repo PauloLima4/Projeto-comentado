2d4ec15655c7a3f27465238fe70ee47aec6dd4d0
PauloLima <159704780+PauloLimaj@users.noreply.github.com>
2024-09-20 07:38:49 -0300

    Update Character.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,20 +1,16 @@
 class Character {
-    #life
+    #life = 1
+    maxLife = 1
+    attack = 0
+    defense = 0
     constructor(name) {
         this.name = name
-        this.#life = 1
-        this.maxLife = 1
-        this.attack = 0
-        this.defense = 0
     }
 
-    // para pegar a vida do char
     get life() {
         return this.#life
     }
 
-    // para enviar a nova vida do char
-    // verificação para que a vida não fique inferior a 0
     set life(newLife) {
         this.#life = newLife < 0 ? 0 : newLife
     }
