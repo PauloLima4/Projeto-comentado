6ba2c4bec1ad336e157eadcc9e105d2833dedd5e
PauloLima <159704780+PauloLimaj@users.noreply.github.com>
2024-09-20 07:36:01 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,22 @@
+let log = new Log(document.querySelector('.log'))
+
 let char = new Knight('John')
 let monster = new LittleMonster()
 
 const stage = new Stage(
-    char,
+    char, 
     monster,
     document.querySelector('#char'),
-    document.querySelector('#monster')
+    document.querySelector('#monster'),
+    log
 )
 
 stage.start()
+
+// console.log(char.name)
+// console.log(char.life)
+// console.log(char.attack)
+
+// console.log(monster.name)
+// console.log(monster.life)
+// console.log(monster.attack)
